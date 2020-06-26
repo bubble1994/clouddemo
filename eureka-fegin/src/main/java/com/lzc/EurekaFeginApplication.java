d4eb69240f5fefbf23842e8e1940b1d5e6dcbdd2
liuzhichao <892556445@qq.com>
2020-06-26 11:41:44 +0800

    fegin commit

---
+++ eureka-fegin/src/main/java/com/lzc/EurekaFeginApplication.java
@@ -0,0 +1,13 @@
+package com.lzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaFeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeginApplication.class, args);
+    }
+
+}
